43c8543dbfab44abfe21658ebd9280c47fe77d25
Fariba Shahsavan <f.shahsavanpour@gmail.com>
2025-03-12 15:34:01 +0330

    script.js & style.css code updated

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
+window.addEventListener("load", function () {
+  showTasks();
+  restoreCheckedTasks();
+});
+
 let input = document.getElementById("input");
 let add = document.getElementById("add");
 let ul = document.getElementById("list");
+let tasks;
+
+if (!localStorage.getItem("todolist")) {
+  tasks = [];
+} else {
+  tasks = getTasks();
+}
 
 function addTask(text) {
   let li = document.createElement("li");
@@ -16,32 +28,74 @@ function addTask(text) {
 
 window.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
-    let text = input.value;
+    let text = input.value.trim();
+    if (text === "" || text === " ") {
+      alert("Please enter a task");
+      return;
+    }
     let task = addTask(text);
     ul.appendChild(task);
+    saveTask(text);
     input.value = "";
   }
 });
 
 add.addEventListener("click", function () {
-  if (input.value === "") {
+  let text = input.value.trim();
+  if (text === "" || text === " ") {
     alert("Please enter a task");
     return;
   }
-  let text = input.value;
   let task = addTask(text);
   ul.appendChild(task);
+  saveTask(text);
   input.value = "";
 });
 
 ul.addEventListener("click", function (e) {
   if (e.target.nodeName === "SPAN") {
     e.target.classList.toggle("checked");
-    e.target.parentElement.classList.toggle("checkBox");
-    e.target.nextElementSibling.style.display = "block";
+    let check = e.target.parentElement;
+    check.classList.toggle("checkBox");
+    saveCheckedTasks();
   }
 
   if (e.target.nodeName === "I") {
-    e.target.parentElement.remove();
+    let target = e.target.parentElement;
+    target.remove();
+    tasks.splice(tasks.indexOf(target.textContent), 1);
+    localStorage.setItem("todolist", tasks);
+    saveCheckedTasks();
   }
 });
+
+function saveTask(text) {
+  tasks.push(text);
+  localStorage.setItem("todolist", tasks);
+}
+function getTasks() {
+  return localStorage.getItem("todolist").split(",");
+}
+function showTasks() {
+  for (const taskText of tasks) {
+    let li = addTask(taskText);
+    ul.appendChild(li);
+  }
+}
+function saveCheckedTasks() {
+  let checkedTasks = [];
+  document.querySelectorAll(".checked").forEach((span) => {
+    checkedTasks.push(span.textContent);
+  });
+  localStorage.setItem("checkedTasks", JSON.stringify(checkedTasks));
+}
+
+function restoreCheckedTasks() {
+  let checkedTasks = JSON.parse(localStorage.getItem("checkedTasks")) || [];
+  document.querySelectorAll("span").forEach((span) => {
+    if (checkedTasks.includes(span.textContent)) {
+      span.classList.add("checked");
+      span.parentElement.classList.add("checkBox");
+    }
+  });
+}
